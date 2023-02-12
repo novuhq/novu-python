ba45b622440c14e9874862838ca4e4c55b057e00
Oscar MARIE--TAILLEFER <oscar.marie-taillefer@spikeelabs.fr>
2023-02-12 23:48:35 +0100

    feat(feeds): setup dto and wrapper

---
+++ novu/api/feed.py
@@ -0,0 +1,49 @@
+"""
+This module is used to define the ``FeedApi``, a python wrapper to interact with ``Feeds`` in Novu.
+"""
+from typing import Iterator, Optional
+
+from novu.api.base import Api
+from novu.constants import FEEDS_ENDPOINT
+from novu.dto.feed import FeedDto
+
+
+class FeedApi(Api):
+    """This class aims to handle all API methods around feeds in Novu"""
+
+    def __init__(self, url: Optional[str] = None, api_key: Optional[str] = None) -> None:
+        super().__init__(url, api_key)
+
+        self._feed_url = f"{self._url}{FEEDS_ENDPOINT}"
+
+    def list(self) -> Iterator[FeedDto]:
+        """List existing feeds
+
+        Yields:
+            Mapped feed
+        """
+        results = self.handle_request("GET", self._feed_url)["data"]
+        for result in results:
+            yield FeedDto.from_camel_case(result)
+
+    def create(self, name: str) -> FeedDto:
+        """Create a new feed using it's name
+
+        Args:
+            name: The name of the feed to create
+
+        Returns:
+            Mapped created feed
+        """
+        return FeedDto.from_camel_case(self.handle_request("POST", self._feed_url, {"name": name})["data"])
+
+    def delete(self, feed_id: str) -> FeedDto:
+        """Remove a feed using it's identifier
+
+        Args:
+            feed_id: The feed identifier
+
+        Returns:
+            Mapped deleted feed
+        """
+        return FeedDto.from_camel_case(self.handle_request("DELETE", f"{self._feed_url}/{feed_id}")["data"])
---
+++ novu/dto/feed.py
@@ -0,0 +1,32 @@
+"""This module is used to gather all DTO definitions related to the Feed resource in Novu"""
+import dataclasses
+from typing import Optional
+
+from novu.dto.base import CamelCaseDto
+
+
+@dataclasses.dataclass
+class FeedDto(CamelCaseDto["FeedDto"]):  # pylint: disable=R0902
+    """Definition of an feed"""
+
+    name: str
+    """Name of the feed"""
+
+    identifier: Optional[str] = None
+    """Feed identifier, should be the given name set by default in Novu"""
+
+    _id: Optional[str] = None
+    """Feed ID in Novu internal storage system"""
+
+    _environment_id: Optional[str] = None
+    """Environment ID in Novu internal storage system"""
+
+    _organization_id: Optional[str] = None
+    """Organization ID in Novu internal storage system"""
+
+    deleted: Optional[bool] = None
+    created_at: Optional[str] = None
+    updated_at: Optional[str] = None
+
+    id: Optional[str] = None  # pylint: disable=C0103
+    """Feed ID in Novu internal storage system"""
