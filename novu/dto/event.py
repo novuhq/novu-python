f9f787e71c70eeba3be0e4b560a62a42e7a5b3c8
Simon Retsch <simon.retsch@nefino.de>
2023-05-10 16:24:53 +0200

    feat: add function to bulk trigger events

--- novu/dto/event.py
+++ novu/dto/event.py
@@ -1,5 +1,6 @@
 """This module is used to gather all DTO definitions related to the Event resource in Novu"""
 import dataclasses
+from typing import List, Optional, Union
 
 from novu.dto.base import CamelCaseDto
 from novu.enums import EventStatus
@@ -17,3 +18,26 @@ class EventDto(CamelCaseDto["EventDto"]):
 
     transaction_id: str
     """Transaction id for trigger."""
+
+
+@dataclasses.dataclass
+class InputEventDto(CamelCaseDto["InputEventDto"]):
+    """Definition of an event used as an input"""
+
+    name: str
+    """The name of the template trigger to activate."""
+
+    recipients: Union[str, List[str]]
+    """A subscriber ID (or a list of subscriber ID) to reach with this trigger."""
+
+    payload: dict
+    """A JSON serializable python dict to pass additional custom information."""
+
+    overrides: Optional[dict] = None
+    """A JSON serializable python dict used to override provider specific configurations."""
+
+    transaction_id: Optional[str] = None
+    """A unique ID for this transaction."""
+
+    actor: Optional[str] = None
+    """It is used to display the Avatar of the provided actor's subscriber id."""
