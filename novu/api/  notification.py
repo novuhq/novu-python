ef56cc6b6fd679c4410c08ff5db072dedc1dfb6d
veldakarimi <veldakarimi@gmail.com>
2023-07-11 22:16:48 +0300

    FEATURE: Add notifications ressource in wrapper #9(update the api and dto)

---
+++ novu/api/  notification.py
@@ -0,0 +1,119 @@
+"""
+This module is used to define the ``NotificationAPI`, a python wrapper
+to interact with ``Notifications`` in Novu.
+"""
+from typing import Optional
+
+import requests
+
+from novu.api.base import Api
+from novu.constants import NOTIFICATIONS
+from novu.dto.notification import NotificationDto
+
+class NotificationApi(Api):
+    """ This class aims to handle all API methods around notifications in API """
+
+    def __init__(
+        self,
+        url: Optional[str] = None,
+        api_key: Optional[str] = None,
+        requests_timeout: Optional[int] = None,
+        session: Optional[requests.Session] = None,
+    ) -> None:
+        super().__init__(url=url, api_key=api_key, requests_timeout=requests_timeout, session=session)
+
+        self._notification_url = f"{self._url}{NOTIFICATIONS}"
+
+    def notification(
+            self,
+            channels: list[str],
+            templates: list[str],
+            emails: list[str],
+            search: str,
+            page: Optional[int] = 0,
+            transaction_id: Optional[str] = None,
+    )-> NotificationDto:
+         """ Trigger an event to get  notifications 
+         Args:
+            channels: is a required parameter and should be an array of strings. It represents the available notification channels, such as "in_app", "email", "sms", "chat", and "push".
+            templates: is a required parameter and should be an array of strings. It represents the notification templates.
+            emails is a required parameter and should be an array of strings. It represents the email addresses associated with the notification.
+            search is a required parameter and should be a string. It represents the search query.
+            page is an optional parameter with a default value of 0. It represents the page number for search results.
+            transactionId is a required parameter and should be a string. It represents the transaction ID associated with the notification.
+
+
+         Returns:
+            Gets notifications in Novu
+
+         """
+         payload = {
+            "channels": channels,
+            "templates": templates,
+            "emails": emails,
+            "search": search,
+            "page": page,
+            "transactionId": transaction_id,
+         }
+         return NotificationDto.from_camel_case(self.handle_request("GET", f"{self._notification_url}/notifications", payload)["data"])
+    
+    def notifications_stats(
+            self,
+            id: Optional[str] = None,
+            start_date: Optional[str] = None,
+            end_date: Optional[str] = None,
+        )-> dict:
+            """ Gets notifications stats 
+            Args:
+                id: is an optional parameter and should be a string. It represents the notification ID.
+                start_date: is an optional parameter and should be a string. It represents the start date for the stats.
+                end_date: is an optional parameter and should be a string. It represents the end date for the stats.
+
+
+            Returns:
+                Gets notifications stats in Novu
+
+            """
+            payload = {
+                "id": id,
+                "start_date": start_date,
+                "end_date": end_date,
+            }
+            response = self.handle_request("GET", f"{self._notification_url}/notifications/stats", payload)
+            return response
+    
+    def notifications_graph_stats(
+        self,
+        id: Optional[str] = None,
+        start_date: Optional[str] = None,
+        end_date: Optional[str] = None,
+        days: Optional[int] = None,
+    )-> dict:
+         """ Gets notifications graph stats 
+         Args:
+            id: is an optional parameter and should be a string. It represents the notification ID.
+            start_date: is an optional parameter and should be a string. It represents the start date for the stats.
+            end_date: is an optional parameter and should be a string. It represents the end date for the stats.
+            days: is an optional parameter and should be an integer. It represents the number of days to get stats for.
+
+
+         Returns:
+            Gets notifications graph stats in Novu
+
+         """
+         payload = {
+            "id": id,
+            "start_date": start_date,
+            "end_date": end_date,
+            "days": days,
+         }
+         response = self.handle_request("GET", f"{self._notification_url}/notification/graph/stats", payload)
+         return response["data"]
+    def check_notification_by_id(
+        self,
+        notification_id: str
+    ) -> NotificationDto:
+        """ Trigger an event to get  notification by id """
+        url = f"{self._notification_url}/{notification_id}"
+        response = self.handle_request("GET", url)
+        return NotificationDto.from_camel_case(response["data"])
---
+++ novu/dto/notification.py
@@ -0,0 +1,161 @@
+"""This module is used to gather all DTO definitions related to the Notifications resource in Novu"""
+import dataclasses
+from typing import List
+
+from novu.dto.base import CamelCaseDto, DtoIterableDescriptor
+
+@dataclasses.dataclass
+class SubscriberDto(CamelCaseDto["SubscriberDto"]):
+        """Definition of  the subscriber """
+
+        first_name: str
+        """The subscriber's first name."""
+
+        _id: str
+        """ The subscriber's unique ID """
+
+        last_name: str
+        """ The subscriber's last name. """
+
+        email: str
+        """ The subscriber's email. """
+
+        phone: str
+        """ The subscriber's phone number """
+
+@dataclasses.dataclass
+class TriggerDto:
+        """Definition of  the triggers """
+
+        type: str
+        """ The type of trigger """
+
+        identifier: str
+        """ The identifier of the trigger """
+
+        variables: List[dict]
+        """ The variables for the trigger """
+
+        subscriber_variables: List[dict]
+        """ The subscriber variables for the trigger """
+  
+@dataclasses.dataclass
+class ExecutionDetailDto:
+        """ Definition of  the execution of the notification """
+
+        _id: str
+        """ The unique ID of the execution """
+
+        _job_id: str
+        """ The ID of the job that executed the notification """
+
+        status: str
+        """ The status of the execution """
+        
+        detail: str
+        """ The detail of the execution """
+
+        is_retry: bool
+        """ Whether the execution is a retry """
+
+        is_test: bool
+        """ Whether the execution is a test   """
+
+        provider_id: dict
+        """ The ID of the provider that executed the notification. """
+
+        raw: str
+        """ The raw data of the execution """
+
+        source: str
+        """ The source of the execution """
+
+@dataclasses.dataclass 
+class StepDto:
+        """ Definition of  the step the notification is in """
+
+        _id: str
+        """ The unique ID of the step. """
+
+        active: bool
+        """ Whether the step is active """
+
+        filters: dict
+        """ The filters for the step """
+
+        template: dict
+        """ The template for the step """
+
+@dataclasses.dataclass 
+class JobDto:
+        """ Definition of  the job that executed the notification """
+
+        _id: str
+        """ The unique ID of the job """
+
+        type: str
+        """ The type of the job """
+
+        digest: dict
+        """ The digest of the job """
+
+        execution_details: List[ExecutionDetailDto]
+        """ The execution details of the job """
+
+        step: StepDto
+        """ The step of the job """
+
+        payload: dict
+        """ The payload of the job """
+
+        provider_id: dict
+        """ The ID of the provider that executed the job """
+
+        status: str
+        """ The status of the job """
+
+@dataclasses.dataclass 
+class TemplateDto:
+        """ Definition of  the template used to send the notification """
+
+        _id: str
+        """ The unique ID of the template """
+
+        name: str
+        """ The name of the template """
+
+        triggers: List[TriggerDto]
+        """ The triggers for the template """
+ 
+
+@dataclasses.dataclass 
+class NotificationDto:
+        """ Definition of  the notification """
+
+        _id: str
+        """ The unique ID of the notification """
+
+        _environment_id: str
+        """ The environment ID of the notification """
+
+        _organization_id: str
+        """ The organization ID of the notification """
+
+        transaction_id: str
+        """ The transaction ID of the notification """
+
+        created_at: str
+        """ The creation date of the notification """
+
+        channels: str
+        """ The channels of the notification """
+
+        subscriber: SubscriberDto
+        """ The subscriber of the notification """
+
+        template: TemplateDto
+        """ The template of the notification """
+
+        jobs: List[JobDto]
+        """ The jobs of the notification """
+ 
